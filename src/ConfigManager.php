cc8efe9d8d7ffc05a42662eb51159837d1576654
LukasBooCode <schu0122@hz.nl>
2026-02-12 13:51:54 +0100

    Assemble ConfigManager.php

--- src/ConfigManager.php
+++ src/ConfigManager.php
@@ -4,4 +4,24 @@ namespace Framework;
 
 class ConfigManager
 {
+    /**
+     * @var string[]
+     */
+    public array $config;
+
+    /**
+     * @param string[] $config
+     */
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
+    public function get(string $key): string
+    {
+        if (!isset($this->config[$key])) {
+            throw new \Exception("Config key not found");
+        }
+        return $this->config[$key];
+    }
 }
